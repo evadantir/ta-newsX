ab89f182849cbfc593e0eeb4167c1677d659875c
evadantir <evadantir@yahoo.com>
2018-05-13 12:40:00 +0700

    clean cnn news

--- extract_int_news.py
+++ extract_int_news.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 import pandas as pd
+import re
 from preprocessing import Preprocess
 
 class ExtractIntNews(object):
@@ -10,52 +11,56 @@ class ExtractIntNews(object):
         return self.pre.normalizePunctuation(text)
 
     def extractContentCSV(self,filename,news,filesave):
-        data = pd.read_csv(filename,sep=',')
+        data = pd.read_csv(filename,sep=';')
+        # print data
 
         #get only needed news
         # data = data.loc[data['publication'] == news]
+
         # #filter needed data
         temp = pd.DataFrame()
-        temp['url'] = data['url']
+        # temp['url'] = data['url']
         temp['date'] = data['date']
-        try:
-            # temp['title'] = data['title']
-            # temp['content'] = data['content']
-            #normalize punctuation
-            temp['title'] = data['title'].apply(lambda x: self.cleansingText(x))
-            # if code == 'CNN':
-            #     temp['content'] = self.cleansingCNN(data['content'])
-            # else:
-            temp['content'] = data['content'].apply(lambda x: self.cleansingText(x))
-        # print data
-        # exit()
-            #save filtered data to CSV
-            temp.to_csv(filesave,sep=',', encoding='utf-8')
-            print "done"
-        except:
-            pass
-    # def cleansingCNN(self, data):
+        # temp['title'] = data['title']
+        # temp['content'] = data['content']
+        #normalize punctuation
+        temp['title'] = data['title'].apply(lambda x: self.cleansingText(x))
+        # if news == 'CNN':
+        temp['content'] = data['content'].apply(lambda x: self.cleansingCNN(x))
+        # elif news == 'Reuters':
+        #     temp['content'] = data['content'].apply(lambda x: self.cleansingReuter(x))
+        # else:
+        #     temp['content'] = data['content'].apply(lambda x: self.cleansingText(x))
+        
+        #save filtered data to CSV
+        temp.to_csv(filesave,sep=';', encoding='utf-8')
+        print "done"
+
+    def cleansingCNN(self, data):
     # pola: Kota - (CNN)
-    #     import re
+        
+        temp = self.cleansingText(data)
+        # print 'done cleansing'
+        temp= re.sub(r'(.*?)\(CNN\) ','',temp)
+        # print 'done remove cnn'
+        return temp
 
-    #     temp = pd.DataFrame()
-    #     temp = self.cleansingText(data)
-    #     temp['content'] = re.sub('')
-
-    # def cleansingReuter(self, data):
+    def cleansingReuter(self, data):
     # pola: ( )
-    #     import re
 
-    #     temp = pd.DataFrame()
-    #     temp = self.cleansingText(data)
-    #     temp['content'] = re.sub('')
+        temp = pd.DataFrame()
+        temp = self.cleansingText(data)
+        temp['content'] = re.sub('')
         
 
 ex = ExtractIntNews()
 
-# ex.extractContentCSV('CNN.csv','CNN','cnn.csv')
+
+# ex.extractContentCSV('CNN.csv','CNN','cnn_only.csv')
+ex.extractContentCSV('cnn_only1.csv','CNN','cnn_edit1.csv')
 # ex.extractContentCSV('Guardian.csv','Guardian','guardian_edit.csv')
 # ex.extractContentCSV('Guardian_Reuter.csv','Reuters','reuters.csv')
-ex.extractContentCSV('reuters.csv','Reuters','reuters_edit.csv')
+# ex.extractContentCSV('reuters.csv','Reuters','reuters_edit.csv')
 # data = pd.read_csv('reuters.csv',sep=';')
 # print data
+# print ex.cleansingCNN('Washington (CNN) Testing')

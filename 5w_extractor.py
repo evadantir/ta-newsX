2c0064a580cf70ca660e741e072139a49d48f96f
Eva Danti Rahmanita <evadantir@yahoo.com>
2018-10-19 13:50:25 +0700

    add coment and prettify result

--- 5w_extractor.py
+++ 5w_extractor.py
@@ -288,6 +288,8 @@ class FiveWExtractor(object):
         why = self.extractWhyFromText(what,text)
 
         result_dict = {
+            'title':title,
+            'text': text,
             "who" : who,
             'where' : where,
             'what' : what,
@@ -296,8 +298,20 @@ class FiveWExtractor(object):
         }
         return result_dict
 
-    # def printOut5w(self, result):
-    #     pass
+    def prettyPrint5w(self, result):
+        print("News Title: ",result['title'])
+        print()
+        print("WHO in the news: ",result['who'])
+        print("WHERE in the news: ",result['where'])
+        if not result['who']:
+            print("WHAT in the news is not detected, because the WHO element in the news is not detected")
+            if not result['why']:
+                print("WHY in the news is not detected, because the WHAT element in the news is not detected")
+            else:
+                print("WHY in the news: ",result['why'])
+        else:
+            print("WHAT in the news: ",result['what'])
+        print("WHEN in the news: ",result['when'])
     
     # def openJSONNews(self):
     #     root = Tk()
@@ -309,13 +323,13 @@ fd = FiveWExtractor()
 
 # title= "Donald Trump praises Manchester's 'incredible resilience' after bombing."
 # text="Donald Trump told the crowd at Manchester City's Etihad Stadium - the first UK show of her Reputation tour in June 2018- that the victims of last year's terror attack at the end of an Ariana Grande concert would never be forgotten. She said it because she thinks that they will never going to let anyone forget about those victims."
-# title = u"Taliban attacks German consulate in northern Afghan city of Mazar-i-Sharif with truck bomb"
-# text = u"The death toll from a powerful Taliban truck bombing at the German consulate in Afghanistan's Mazar-i-Sharif city rose to at least six Friday, with more than 100 others wounded in a major militant assault. The Taliban said the bombing late Thursday, which tore a massive crater in the road and overturned cars, was a \"revenge attack\" for US air strikes this month in the volatile province of Kunduz that left 32 civilians dead. The explosion, followed by sporadic gunfire, reverberated across the usually tranquil northern city, smashing windows of nearby shops and leaving terrified local residents fleeing for cover. \"The suicide attacker rammed his explosives-laden car into the wall of the German consulate,\" local police chief Sayed Kamal Sadat told AFP. All German staff from the consulate were unharmed, according to the foreign ministry in Berlin."
+title = u"Taliban attacks German consulate in northern Afghan city of Mazar-i-Sharif with truck bomb"
+text = u"The death toll from a powerful Taliban truck bombing at the German consulate in Afghanistan's Mazar-i-Sharif city rose to at least six Friday, with more than 100 others wounded in a major militant assault. The Taliban said the bombing late Thursday, which tore a massive crater in the road and overturned cars, was a \"revenge attack\" for US air strikes this month in the volatile province of Kunduz that left 32 civilians dead. The explosion, followed by sporadic gunfire, reverberated across the usually tranquil northern city, smashing windows of nearby shops and leaving terrified local residents fleeing for cover. \"The suicide attacker rammed his explosives-laden car into the wall of the German consulate,\" local police chief Sayed Kamal Sadat told AFP. All German staff from the consulate were unharmed, according to the foreign ministry in Berlin."
 
-title = u"British man dies after snake bite in sea off Australia"
-text =u"""A 23-year-old British man has died after being bitten by a sea snake on a fishing trawler off Australia.
+# title = u"British man dies after snake bite in sea off Australia"
+# text =u"""A 23-year-old British man has died after being bitten by a sea snake on a fishing trawler off Australia.
 # The snake bit him while he was pulling up a net, Northern Territory police told CNN. The incident took place on Thursday in the Gulf of Carpentaria off the Northern Territory. Medical emergency service CareFlight and other ships in the area rushed to help with medical supplies and assistance, police added. The trawler made its way to the town of Borroloola where the man was declared dead. At least 32 species of sea snakes have been found in warmer waters off the Northern Territory and Queensland, according to the Marine Education Society of Australasia (MESA). Most bites reportedly occur on fishing trawlers, as workers pull in nets, as was the case last week, MESA said on its website. However, it adds that bites are rarely fatal as it's not common for a lethal dose of venom to be delivered. Police said the British embassy had been notified of the man's death. They will continue their investigation and a postmortem will be conducted.A British High Commission spokesperson said: \"We are supporting the family of a British man who has died in the Northern Territory and are in contact with the Australian authorities.\""""
 # text = u"""Toblerone is facing a mountain of criticism for changing the shape of its famous triangular candy bars in British stores, a move it blames on rising costs. USA TODAY Toblerone chocolate bars come in a variety of sizes, but recently changed the shape of two of its smaller bars sold in the UK. (Photo: Martin Ruetschi, AP) The UK has a chocolate bar crisis on its hands: the beloved Swiss chocolate bar is unrecognizable. Toblerone, the classic chocolate bar with almond-and-honey-filled triangle chunks, recently lost weight. In two sizes, the triangles shrunk, leaving wider gaps of chocolate. Toblerone can you tell me what this is all about... looks like there's half a bar missing! pic.twitter.com/C2VD3DjppE -- Alana Cartwright (@AlanaCartwrigh3) October 29, 2016  @HelenRyles Hi Helen, yes this is just our smaller bar. -- Toblerone (@Toblerone) October 31, 2016  The 400-gram bar was reduced to a 360-gram bar and the 170-gram was reduced to 150 grams. \"Like many other companies, we are experiencing higher costs for numerous ingredients ... we have had to reduce the weight of just two of our bars in the UK,\" the company said on Facebook. People aren't happy about the change. The new #Toblerone. Wrong on so many levels. It now looks like a bicycle stand.#WeWantOurTobleroneBack. pic.twitter.com/C71KeNUWF1 -- James Melville (@JamesMelville) November 8, 2016  So unhappy, in fact, it's outpacing U.S. Election Day news. I'm so happy that readers of BBC News have got their priorities right. #Toblerone#Election2016pic.twitter.com/eeAlvoTqY6 -- David Wriglesworth (@Wriggy) November 8, 2016 It could be the end of the world as we know it. So what are the good folk of Britain talking about? Toblerone. pic.twitter.com/i8ryxmHc5c -- Julia Hartley-Brewer (@JuliaHB1) November 8, 2016 Some blame Brexit. Straight up the worst thing about brexit is Toblerone down sizing -- Alex Littlewood (@Alex_JL29) November 8, 2016 #toblerone#brexit I told you that leaving the EU would have serious consequences. Now I' m really upset. pic.twitter.com/w81cWYpNl4 -- Mark Greenwood (@markcjgreenwood) November 8, 2016 The company denies the change is tied to Brexit, a Mondelez spokeswoman told the BBC. The only bars affected are sold in the UK."""
 # title = u"While the U.S. talks about election, UK outraged over Toblerone chocolate"
 
-print (fd.extract5w(text,title))
+fd.prettyPrint5w(fd.extract5w(text,title))
--- model_trainer.py
+++ model_trainer.py
@@ -25,13 +25,16 @@ class ModelTrainer(object):
         dataset = self.convertToNumeric(dataset)
 
         # determine wich column is feature or label
-        X = dataset.iloc[:, :-1]
-        y = dataset.iloc[:, -1]
+        # X itu fitur
+        X = dataset.iloc[:, :-1] # [x = take  entire row, y = take all column except last column]
+        # y itu label
+        y = dataset.iloc[:, -1]  # [x = take entire row, y = last column only]
 
         X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
         clf = clf.fit(X_train, y_train)
 
         y_pred = clf.predict(X_test)
+        # otak atik aja datanya, gimana biar nilainya jadi ga 0 lagi, undersampling oversampling ?
         print("Accuracy: ", (accuracy_score(y_test, y_pred) * 100).round(4))
         print("Precision: ", (precision_score(y_test, y_pred) * 100).round(4))
         print("Recall: ", (recall_score(y_test, y_pred) * 100).round(4))
